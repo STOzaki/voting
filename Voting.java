7531899e365bc781386864fa2e59537e0c237f90
Salem Ozaki <sozaki19@csl10-l.cornellcollege.edu>
2018-02-28 23:32:21 -0600

    finished the first part of the voting system.

---
+++ Voting.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voting;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+/**
+ *
+ * @author sozaki19
+ */
+public class Voting {
+    JSONParser parser = new JSONParser();
+
+        
+}
